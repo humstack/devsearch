ef1557895a4bbdfbbc95adea6e24007e44718499
Humphrey Menge <humphreymenge@gmail.com>
2022-05-21 13:23:05 +0300

    Second commit

--- search/settings.py
+++ search/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "*.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "softwaredevsearch.herokuapp.com"]
 
 
 # Application definition
